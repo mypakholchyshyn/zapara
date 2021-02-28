a911441bf468d15874a88ca09bec4c27d6217bf2
mypakholchyshyn <64320032+mypakholchyshyn@users.noreply.github.com>
2021-02-28 18:59:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const Todo = props => (
+    <li>
+        <input type="checkbox" checked={props.todo.check} onChange={props.onChange} />
+        <button onClick={props.onDelete}>delete</button>
+        <span>{props.todo.text}</span>
+    </li>
+)
+
+let id = 0;
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: []
+        }
+    }
+
+    addTodo() {
+        const text = prompt("add todo");
+        this.setState({ todos: [...this.state.todos, { id: id++, text: text, check: false }] });
+    }
+
+    deleteTodo(id){
+        this.setState({
+            todos: this.state.todos.filter(todo => todo.id !== id)
+        })
+    }
+
+    changeTodo(id){
+        this.setState({ todos: this.state.todos.map(todo => todo.id == id ? { ...todo, check: !todo.check } : todo) })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>My Todo App</h2>
+                <div>Todo count: {this.state.todos.length}</div>
+                <div>Unchecked Todo count: {this.state.todos.filter(todo => !todo.check).length}</div>
+                <button onClick={() => this.addTodo()}>Add todo</button>
+                <ul>
+                    {this.state.todos.map(todo => <Todo 
+                    onDelete ={() => this.deleteTodo(todo.id)} 
+                    onChange ={()=> this.changeTodo(todo.id)} todo={todo} />)}
+                </ul>
+                
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
